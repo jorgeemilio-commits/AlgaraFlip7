6a5adc66d329f5a47c2888d6c612044f0e25a05a
emilio_com <jorgeemilio7777@gmail.com>
2025-12-03 15:53:21 -0700

    Se crearon nuevos estados para el menu de las salas

--- servidorhilos/src/main/java/com/servidormulti/EstadoMenu.java
+++ servidorhilos/src/main/java/com/servidormulti/EstadoMenu.java
@@ -8,4 +8,8 @@ public enum EstadoMenu {
     REGISTRO_PEDIR_CONTRASENA, // Esperando contraseña para Registrar
     REGISTRO_PEDIR_CONFIRMACION, // Esperando confirmación de contraseña
     MENU_CHAT, // Usuario logueado o invitado en modo chat
+    MENU_SALA_PRINCIPAL,    // Menú de salas (Opciones: Unirse/Crear/Logout)
+    MENU_UNIRSE_SALA,       // Esperando el nombre de la sala para unirse
+    MENU_CREAR_SALA_NOMBRE, // Esperando el nombre de la sala a crear
+    SALA_ACTIVA,            // El cliente está activamente chateando en una sala
 }
