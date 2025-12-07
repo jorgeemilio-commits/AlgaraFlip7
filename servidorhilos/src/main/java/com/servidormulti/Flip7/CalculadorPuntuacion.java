a2d91c86e0d301274246c1d845671322a0a4b7e0
fabian <blackccks020@gmail.com>
2025-12-06 18:15:14 -0700

    clase para calcular puntuacion en mano con su logica de puntos terminada

--- servidorhilos/src/main/java/com/servidormulti/Flip7/CalculadorPuntuacion.java
+++ servidorhilos/src/main/java/com/servidormulti/Flip7/CalculadorPuntuacion.java
@@ -1,5 +1,39 @@
 package com.servidormulti.Flip7;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class CalculadorPuntuacion {
-    
+    public int calcularPuntuacion(List<Carta> mano) {
+        int sumaBase = 0;
+        int multiplicador = 1;
+        int bonoPuntos = 0;
+        
+        Set<Integer> valoresUnicos = new HashSet<>();
+
+        for (Carta c : mano) {
+            if (c.obtenerTipo() == TipoCarta.NUMERICA) {
+                sumaBase += c.obtenerValor();
+                valoresUnicos.add(c.obtenerValor());
+            } else if (c.obtenerTipo() == TipoCarta.BONUS) {
+                if (c.toString().equals("x2")) {
+                    multiplicador *= 2;
+                } else if (c.toString().contains("+10")) { 
+                    bonoPuntos += 10;
+                }
+            }
+        }
+
+
+        int total = sumaBase * multiplicador;
+
+        total += bonoPuntos;
+
+        if (valoresUnicos.size() >= 7) {
+            total += 15; 
+        }
+
+        return total;
+    }
 }
